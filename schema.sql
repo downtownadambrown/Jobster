b532d032c70898b0bece3a43bcfed604a88c17bb
Pamela Anderson <pamelaanderson519@yahoo.com>
2018-11-13 11:10:42 -0500

    added the html login for manager and applicant

--- schema.sql
+++ schema.sql
@@ -2,3 +2,14 @@
 DROP DATABASE IF EXISTS jobster_db;
 -- Creates the "bamazon" database --
 CREATE DATABASE jobster_db;
+
+USE jobster_db;
+
+Create table regi(
+     id int AUTO_INCREMENT PRIMARY KEY,
+     uname varchar(50) NOT NULL,
+     email varchar(80) NOT NULL,
+     passwd varchar(50) NOT NULL,
+     fname varchar(30) NOT NULL,
+     gender varchar(20) NOT NULL,
+);
