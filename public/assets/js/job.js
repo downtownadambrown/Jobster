077436d2e93cc91e727803a61724dbd4d5f7c66d
Pamela Anderson <pamelaanderson519@yahoo.com>
2018-11-14 17:06:09 -0500

    readding this push

--- public/assets/js/job.js
+++ public/assets/js/job.js
@@ -2,20 +2,20 @@
 //DO NOT DELETE THIS PAGE
 
 
-(function ($) {
-    const jobId = localStorage.getItem("userId");
-    $.get(`/api/job/${jobId}`)
-        .then(function (data) {
-            const job = data[0];
+// (function ($) {
+//     const jobId = localStorage.getItem("userId");
+//     $.get(`/api/job/${jobId}`)
+//         .then(function (data) {
+//             const job = data[0];
 
-            console.log("data inside get /api/job promise", data);
+//             console.log("data inside get /api/job promise", data);
 
-            //Append the information to the page
+//             //Append the information to the page
 
-            $('#manager-job-position').text(job.position);
-            $('#manager-job-rate').text(job.hourlyRate);
-            $('#manager-job-start').text(job.startDate);
-            $('#manager-job-hours').text(job.hours);
-            $('#manager-job-zip').text(job.zipcode);
-        });
-})(jQuery);
+//             $('#manager-job-position').text(job.position);
+//             $('#manager-job-rate').text(job.hourlyRate);
+//             $('#manager-job-start').text(job.startDate);
+//             $('#manager-job-hours').text(job.hours);
+//             $('#manager-job-zip').text(job.zipcode);
+//         });
+// })(jQuery);
